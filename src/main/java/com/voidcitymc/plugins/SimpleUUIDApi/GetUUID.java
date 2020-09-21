53857ee5229cb852df400efcd2045b65274e7627
ramdon-person <fierceeo@gmail.com>
2020-09-20 20:21:09 -0700

    using essentials uuid lookup system

--- src/main/java/com/voidcitymc/plugins/SimpleUUIDApi/GetUUID.java
+++ src/main/java/com/voidcitymc/plugins/SimpleUUIDApi/GetUUID.java
@@ -1,15 +1,18 @@
 package com.voidcitymc.plugins.SimpleUUIDApi;
 
+import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.User;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.json.JSONObject;
 
 
+
 public class GetUUID {
     public static String getUUID(String player) {
-        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(player);
-        if (offlinePlayer.hasPlayedBefore()) {
-            return offlinePlayer.getUniqueId().toString();
+        Essentials essentialsMain = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
+        if (essentialsMain != null && essentialsMain.getOfflineUser(player) != null) {
+            return essentialsMain.getOfflineUser(player).getConfigUUID().toString();
         }
         //if luckperms is false use mojang then.
         return mojangUUIDLookup(player);
