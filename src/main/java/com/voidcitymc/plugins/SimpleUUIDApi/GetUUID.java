b8abe949768bc4133917400c29ef30bb008472a4
fierceeo <fierceeo@gmail.com>
2020-09-22 21:05:26 -0700

    add debug

--- src/main/java/com/voidcitymc/plugins/SimpleUUIDApi/GetUUID.java
+++ src/main/java/com/voidcitymc/plugins/SimpleUUIDApi/GetUUID.java
@@ -9,22 +9,24 @@ import org.json.JSONObject;
 public class GetUUID {
     public static String getUUID(String player, String token) {
         Essentials essentialsMain = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
+        System.out.println("ess");
         if (essentialsMain != null && essentialsMain.getOfflineUser(player).getConfigUUID() != null) {
             return essentialsMain.getUserMap().getUser(player).getConfigUUID().toString();
         }
         //if essentials is false use mojang then.
         if (player.charAt(0) != '-') {
+            System.out.println("mo");
             String mojang = mojangUUIDLookup(player);
             if (mojang != null) {
                 return mojang;
             }
         }
         //if mojang is false then use bedrock
+        System.out.println("bed");
         String bedrock = GetJsonText.readtextFromUrl("https://xapi.us/v2/xuid/"+player.replaceFirst("-", ""), token);
-        if (bedrock != null) {
-            return bedrock;
-        }
-        return null;
+        System.out.println(bedrock);
+        return bedrock;
+
     }
 
 
